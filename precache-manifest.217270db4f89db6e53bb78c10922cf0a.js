f9247013d48130b57d4f0dbddec6d2df60a76bc6
Andres Sciacca <andres.sciacca@hasar.com>
2020-07-06 12:06:46 -0300

    Updates

---
+++ precache-manifest.217270db4f89db6e53bb78c10922cf0a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a828f20bc0386cfdc9431d4ad310f494",
+    "url": "/Memotest/index.html"
+  },
+  {
+    "revision": "c4aa9e07cfbe70f620f5",
+    "url": "/Memotest/static/css/2.486e11d6.chunk.css"
+  },
+  {
+    "revision": "e7697dc9ae123d644d40",
+    "url": "/Memotest/static/css/main.92abf971.chunk.css"
+  },
+  {
+    "revision": "c4aa9e07cfbe70f620f5",
+    "url": "/Memotest/static/js/2.49aa455f.chunk.js"
+  },
+  {
+    "revision": "e7697dc9ae123d644d40",
+    "url": "/Memotest/static/js/main.b1a877ac.chunk.js"
+  },
+  {
+    "revision": "1dc2a116b6ab7ef6b84e",
+    "url": "/Memotest/static/js/runtime~main.881b3563.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Memotest/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Memotest/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Memotest/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Memotest/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Memotest/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
